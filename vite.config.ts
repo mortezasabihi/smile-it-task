e5f9a52c23d1f0cc26cd2695184fce6f4e506c16
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-07-15 12:47:09 +0330

    style: fix project code formats with the eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [vue()],
 })
