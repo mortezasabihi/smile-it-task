28038c42b09aac4fd9223a7e5a620163c8dffa15
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-07-15 12:57:00 +0330

    chore: install tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
