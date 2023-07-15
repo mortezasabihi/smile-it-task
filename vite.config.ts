0fecbc78eb73d6261210bd8736db78fb4c5d1e88
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-07-15 19:02:54 +0330

    chore: install vite svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
 })
